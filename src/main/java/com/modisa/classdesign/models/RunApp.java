f44a0cd4a4c4870b1f1e5642b929b13a0dc337bf
mmokgethi <modisa@ModisaM.localdomain>
2019-10-25 08:53:16 +0200

    Comple Generics and Collections

--- src/main/java/com/modisa/classdesign/models/RunApp.java
+++ src/main/java/com/modisa/classdesign/models/RunApp.java
@@ -30,62 +30,6 @@ public class RunApp {
     public static void main(String[] args) {
         System.out.println("Everything is working fine!!!!");
 
-        /*_______________________[Generics and Collections]__________________________________________________________________________________________*/
 
-        // Generic Classes
-        System.out.println("Generic Classes starts here: ");
-
-        BoxPrinter<Integer> val1 = new BoxPrinter<Integer>(new Integer(10));
-        System.out.println(val1);
-
-        BoxPrinter<String> val2 = new BoxPrinter<String>(new String("Generic Class working"));
-        System.out.println(val2);
-
-        Pair<Integer, String> pair = new Pair<Integer, String>(2010, "South Africa");
-        System.out.println("World Cup " + pair.getVal2() + " in " + pair.getVal1());
-
-        // Raw Types
-        RawTest rawTest = new RawTest();
-        rawTest.myTest();
-        rawTest.myTest2();
-
-        // Generic Methods
-        Utilities utilities = new Utilities();
-        utilities.testFill();
-
-        // Generic Subtyping and Wildcard Parameters
-        TestPurpose testPurpose = new TestPurpose();
-        testPurpose.fillList();
-
-        // Collection Classes [Abstract Classes and Interface]
-        CollectionClass collectionClass = new CollectionClass();
-        collectionClass.collClass();
-
-        TreeSets treeSets = new TreeSets();
-        treeSets.treeSet();
-
-        // Map Interface
-        NavigableMapTest navigableMapTest = new NavigableMapTest();
-        navigableMapTest.navigateMapTest();
-
-        // The Deque Interface and ArrayDeque class
-        SplQueue splQueue = new SplQueue();
-        splQueue.testHere();
-
-        //Comparable and Comparator Interfaces
-        Student student[] = {new Student("211365102", "Modisa", 3.1),
-                                new Student("211365103", "Malusi", 3.4),
-                                new Student("211365104", "Phomolo", 2.7),
-                                new Student("211365105", "Sphelele", 3.7)};
-
-        System.out.println("Before sorting by student ID");
-        System.out.println("Student-ID \t Name \t CGPA (for 4.0)");
-        System.out.println(Arrays.toString(student));
-
-        Arrays.sort(student);
-
-        System.out.println("After sorting by student ID");
-        System.out.println("Student-ID \t Name \t CGPA (for 4.0)");
-        System.out.println(Arrays.toString(student));
     }
 }
